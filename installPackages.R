985c037f7fbcec4cb39c259bf17a6a50ea210167
Das <piyali.das@osumc.edu>
2019-07-01 16:37:51 -0400

    Survey dashboard

---
+++ installPackages.R
@@ -0,0 +1,23 @@
+# Description: This code is for listing all the packages that needs to be installed for OMAS dashboard. 
+# Author: Piyali Das
+# Modified By: Piyali Das
+# Date Created: Oct 07, 2017
+# Date Modified: Sep 27, 2018
+
+
+install.packages("shiny")
+install.packages("rmarkdown")
+install.packages("flexdashboard")
+install.packages("DT")
+install.packages("survey")
+install.packages("magrittr")
+install.packages("shinyBS")
+install.packages("srvyr")
+install.packages("plotly")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("forcats")
+install.packages("janitor")
+install.packages("tools")
+install.packages("leaflet.minicharts")
+install.packages("sf")
